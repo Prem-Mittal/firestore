219d034c28192130008abc776cc259400ddd515c
Prem <premmittal88@gmail.com>
2023-09-18 22:13:04 +0530

    firstore databse completed

--- src/App.js
+++ src/App.js
@@ -2,53 +2,94 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import { app,database } from "./firebaseConfig";
 import {
-  getAuth,
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged,
-  createUserWithEmailAndPassword,
-  GoogleAuthProvider,
-  signInWithPopup,
-  GithubAuthProvider
-} from "firebase/auth";
+  collection,addDoc,getDocs, updateDoc,doc,deleteDoc
+} from "firebase/firestore";
 
 function App() {
-  const auth = getAuth();
-  const googleProvider=new GoogleAuthProvider();
-  const provider = new GithubAuthProvider();
+  // const auth = getAuth();
+  // const googleProvider=new GoogleAuthProvider();
+  // const provider = new GithubAuthProvider();
   const [data, setData] = useState({
     email: "",
     password: "",
   });
+
+  const collectionRef=collection(database,'users');
+
   const handleInputs = (event) => {
     let inputs = { [event.target.name]: event.target.value };
 
     setData({ ...data, ...inputs });
   };
 
-  const addData = () => {
-    signInWithPopup(auth, googleProvider)
-    .then((response)=>{
-      console.log(response.user);
-    })
-    .catch((err)=>{
-      alert(err.message);
-    });
-  };
+  // const addData = () => {
+  //   signInWithPopup(auth, googleProvider)
+  //   .then((response)=>{
+  //     console.log(response.user);
+  //   })
+  //   .catch((err)=>{
+  //     alert(err.message);
+  //   });
+  // };
 
   const handlelogout = () => {
-    signOut(auth);
+   addDoc(collectionRef,{
+    email:data.email,
+    password:data.password
+   })
+   .then(()=>{
+    alert('data added');
+   })
+   .catch((err)=>{
+    alert(err.message);
+   })
   };
 
-  useEffect(() => {
-    onAuthStateChanged(auth, (data) => {
-      if (data) {
-        alert("Logged In");
-      } else {
-        alert("Not Logged In");
-      }
-    });
-  }, []);
+  const getdata=()=>{
+    getDocs(collectionRef).then((response)=>{    //This line uses the getDocs function to fetch all the documents in the Firestore collection referenced by collectionRef.
+      console.log(response.docs.map((item)=>{
+        return {...item.data(),id:item.id}; //This line maps over the array of documents (response.docs) and transforms each document into an object. For each document, it combines the document data (item.data()) with an additional property id containing the document's ID (item.id).
+      }))
+    })
+  }
+  
+  const updatedata=()=>{
+    const docToUpdate=doc(database,"users",'3RxJJKNN1tLHT2eUH6sQ'); //This line creates a reference to the document you want to update. It specifies the Firestore database (database), the collection name ("users"), and the document ID ('3RxJJKNN1tLHT2eUH6sQ').
+    updateDoc(docToUpdate,{
+      email:'ABC',
+      password:'1234'
+    })
+    .then(()=>{
+      alert('data updated');
+     })
+     .catch((err)=>{
+      alert(err.message);
+     })
+  }
+
+  const deletedata=()=>{
+    const docToUpdate=doc(database,"users",'3RxJJKNN1tLHT2eUH6sQ'); //This line creates a reference to the document you want to update. It specifies the Firestore database (database), the collection name ("users"), and the document ID ('3RxJJKNN1tLHT2eUH6sQ').
+    deleteDoc(docToUpdate,{
+      email:'ABC',
+      password:'1234'
+    })
+    .then(()=>{
+      alert('data updated');
+     })
+     .catch((err)=>{
+      alert(err.message);
+     })
+  }
+
+  // useEffect(() => {
+  //   onAuthStateChanged(auth, (data) => {
+  //     if (data) {
+  //       alert("Logged In");
+  //     } else {
+  //       alert("Not Logged In");
+  //     }
+  //   });
+  // }, []);
   return (
     <div className="App-header">
       <input
@@ -66,8 +107,8 @@ function App() {
         onChange={(event) => handleInputs(event)}
       />
 
-      <button onClick={addData}>Log In</button>
-      <button onClick={handlelogout}>Log out</button>
+      {/* <button onClick={addData}>Log In</button> */}
+      <button onClick={deletedata}>Log out</button>
     </div>
   );
 }
